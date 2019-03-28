09befe07b7de28dda303355b0539cde78080f410
Caleb Sherfick <calebsherfick@gmail.com>
2019-03-27 22:29:23 -0600

    finished

--- Program.cs
+++ Program.cs
@@ -9,80 +9,88 @@ namespace RockPaperScissors
       Console.Clear();
       System.Console.WriteLine("Lets play Rock | Paper | Scissors!");
       Random mathRandomTool = new Random();
-      System.Console.WriteLine(@"WHAT DO YOU CHOOSE?
-      1 = ROCK
-      2 = PAPER
-      3 = SCISSORS");
-      string PChoice = Console.ReadLine().ToLower();
-      int Random = mathRandomTool.Next(1, 4);
+      bool playing = true;
       int CompWins = 0;
       int PWins = 0;
 
-
-      switch (Random)
+      while (playing)
       {
-        case 1:
-          if (PChoice == "1")
-          {
-            System.Console.WriteLine("You both chose ROCK");
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          else if (PChoice == "2")
-          {
-            System.Console.WriteLine("PAPER beats ROCK: You WON!");
-            PWins++;
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          else if (PChoice == "3")
-          {
-            System.Console.WriteLine("ROCK beats SCISSORS: You LOST!");
-            CompWins++;
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          break;
-        case 2:
-          if (PChoice == "1")
-          {
-            System.Console.WriteLine("PAPER beats ROCK: You LOST!");
-            CompWins++;
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          else if (PChoice == "2")
-          {
-            System.Console.WriteLine("You both chose PAPER");
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          else if (PChoice == "3")
-          {
-            System.Console.WriteLine("SCISSORS beats PAPER: You WON!");
-            PWins++;
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          break;
-        case 3:
-          if (PChoice == "1")
-          {
-            System.Console.WriteLine("ROCK beats SCISSORS: You WON! ");
-            PWins++;
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          else if (PChoice == "2")
-          {
-            System.Console.WriteLine("PAPER beats ROCK: You LOST! ");
-            CompWins++;
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          else if (PChoice == "3")
-          {
-            System.Console.WriteLine("You both chose SCISSORS");
-            System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
-          }
-          break;
+        System.Console.WriteLine(@"WHAT DO YOU CHOOSE?
+      1 = ROCK
+      2 = PAPER
+      3 = SCISSORS");
+
+        int PChoice;
+        Int32.TryParse(Console.ReadLine(), out PChoice);
+        int Random = mathRandomTool.Next(1, 4);
+
+        if (PChoice < 1 || PChoice > 3)
+        {
+          System.Console.WriteLine("Enter 1, 2, or 3 for your choice.");
+          continue;
+        }
+
+        switch (Random)
+        {
+          case 1:
+            if (PChoice == 1)
+            {
+              System.Console.WriteLine("You both chose ROCK");
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            else if (PChoice == 2)
+            {
+              System.Console.WriteLine("PAPER beats ROCK: You WON!");
+              PWins++;
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            else if (PChoice == 3)
+            {
+              System.Console.WriteLine("ROCK beats SCISSORS: You LOST!");
+              CompWins++;
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            break;
+          case 2:
+            if (PChoice == 1)
+            {
+              System.Console.WriteLine("PAPER beats ROCK: You LOST!");
+              CompWins++;
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            else if (PChoice == 2)
+            {
+              System.Console.WriteLine("You both chose PAPER");
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            else if (PChoice == 3)
+            {
+              System.Console.WriteLine("SCISSORS beats PAPER: You WON!");
+              PWins++;
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            break;
+          case 3:
+            if (PChoice == 1)
+            {
+              System.Console.WriteLine("ROCK beats SCISSORS: You WON! ");
+              PWins++;
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            else if (PChoice == 2)
+            {
+              System.Console.WriteLine("PAPER beats ROCK: You LOST! ");
+              CompWins++;
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            else if (PChoice == 3)
+            {
+              System.Console.WriteLine("You both chose SCISSORS");
+              System.Console.WriteLine($"WINS: {PWins} LOSSES: {CompWins}");
+            }
+            break;
+        }
       }
-
-
-
-
     }
   }
 }
